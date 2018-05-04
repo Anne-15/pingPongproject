b826f4d57f0977c7aace3ce9f8bd9d82b4da94b3
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-04 15:15:25 +0300

    add java script to the file project

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
 //business logic
-function operation;
-for (var i = 1, i <= integer, i++) {
-  if (i % 15 == 0) {
-    $("#result").append("<li>" + "pingpong" + "</li>")
-  } else if (i % 5 == 0) {
-    $("#result").append("<li>" + "pong" + "</li>")
-  } else if (i % 3 == 0) {
-    $("#result").append("<li>" + "ping" + "</li>")
-  } else {
-    $("#result").append("<li>" + i.toString() + "</li>");
+var number;
+
+function condition() {
+  for (var count = 1; count <= number; count++) {
+    if (count % 15 === 0) {
+      $("#result").append("<li>" + "pingpong" + "</li>");
+    } else if (count % 5 === 0) {
+      $("#result").append("<li>" + "pong" + "</li>");
+    } else if (count % 3 === 0) {
+      $("#result").append("<li>" + "ping" + "</li>");
+    } else {
+      $("#result").append("<li>" + count.toString() + "</li>");
+    }
   }
 }
 //user interface logic
 $(document).ready(function() {
-  $("form.pingpong").submit(function() {
+  $("form#pingpong").submit(function(event) {
     number = parseInt($("#a").val());
     $("#result").text("");
-
-
-    $("#result").val('');
+    condition();
+    $("#result").show();
+    $("#a").val('');
     event.preventDefault();
   });
 });
